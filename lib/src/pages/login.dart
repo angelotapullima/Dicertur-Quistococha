48ae6ef738f1aaca4b9b9f1b56ef4ebc46ff76f7
anked10 <angelo.anked@gmail.comm>
2022-01-05 12:46:36 -0500

    changes de namespaces Android - IOS

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:dicertur_quistococha/src/api/login_api.dart';
+import 'package:dicertur_quistococha/src/utils/auth.dart';
 import 'package:dicertur_quistococha/src/utils/utils.dart';
 import 'package:dicertur_quistococha/src/widget/show_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -13,15 +17,11 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerpassword = TextEditingController();
   bool? _passwordVisible;
 
-
-
   final _controller = ControllerLogin();
-
 
   @override
   void initState() {
@@ -190,7 +190,7 @@ class _LoginState extends State<Login> {
                   ],
                 ),
                 SizedBox(
-                  height: ScreenUtil().setHeight(132),
+                  height: ScreenUtil().setHeight(50),
                 ),
                 SizedBox(
                   width: double.infinity,
@@ -198,30 +198,25 @@ class _LoginState extends State<Login> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    onPressed: () async{
-                      
+                    onPressed: () async {
                       if (_controllerEmail.text.length > 0) {
-                            if (_controllerpassword.text.length > 0) {
-                              _controller.changeLoadding(true);
-                              final _login = LoginApi();
-                              final res = await _login.login(_controllerEmail.text, _controllerpassword.text);
+                        if (_controllerpassword.text.length > 0) {
+                          _controller.changeLoadding(true);
+                          final _login = LoginApi();
+                          final res = await _login.login(_controllerEmail.text, _controllerpassword.text);
 
-                              if (res.code == '1') {
-                                Navigator.of(context).pushNamedAndRemoveUntil('home', (Route<dynamic> route) => false);
-                              } else {
-                                showToast2(res.message!, Colors.black);
-                              }
-                              _controller.changeLoadding(false);
-                            } else {
-                              showToast2('Ingrese su contraseña', Colors.black);
-                            }
+                          if (res.code == '1') {
+                            Navigator.of(context).pushNamedAndRemoveUntil('home', (Route<dynamic> route) => false);
                           } else {
-                            showToast2('Ingrese su usuario', Colors.black);
+                            showToast2(res.message!, Colors.black);
                           }
-
-
-
-
+                          _controller.changeLoadding(false);
+                        } else {
+                          showToast2('Ingrese su contraseña', Colors.black);
+                        }
+                      } else {
+                        showToast2('Ingrese su usuario', Colors.black);
+                      }
                     },
                     color: Color(0xffffb240),
                     child: Text('Ingresar'),
@@ -229,7 +224,7 @@ class _LoginState extends State<Login> {
                   ),
                 ),
                 SizedBox(
-                  height: ScreenUtil().setHeight(80),
+                  height: ScreenUtil().setHeight(20),
                 ),
                 InkWell(
                   onTap: () {
@@ -244,6 +239,273 @@ class _LoginState extends State<Login> {
                     ),
                   ),
                 ),
+                Text(
+                  'O Iniciar Sesión con',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: ScreenUtil().setSp(19),
+                  ),
+                ),
+                SizedBox(
+                  height: ScreenUtil().setHeight(4),
+                ),
+                (Platform.isAndroid)
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () async {
+                              final user = await Auth.instance.signInWithGoogle(context);
+
+                              if (user.emailAddress!.isNotEmpty) {
+                                print('${user.name}');
+                                print('${user.emailAddress}');
+                                final _login = LoginApi();
+
+                                final ApiModel code = await _login.loginEmail('${user.emailAddress}');
+                                /*  final bottomBloc = ProviderBloc.bottom(context);
+                                  bottomBloc.changePage(0); */
+                                if (code.code == '1') {
+                                  /*  String token;
+                                      final tokenApi = TokenApi();
+                                      final preferences = Preferences();
+                                      token = await FirebaseMessaging.instance.getToken();
+                                      tokenApi.enviarToken(token);
+                                      preferences.tokenFirebase = token; */
+                                  Navigator.of(context).pushNamedAndRemoveUntil('home', (Route<dynamic> route) => false);
+                                  //Navigator.pushReplacementNamed(context, 'home');
+
+                                } else {
+                                  String nome = '';
+                                  String mail = '';
+                                  if (user.name != null || user.name!.isNotEmpty) {
+                                    nome = user.name!;
+                                  }
+                                  if (user.emailAddress != null || user.emailAddress!.isNotEmpty) {
+                                    mail = user.emailAddress!;
+                                  }
+
+                                  print('ptmr');
+                                  /*  Navigator.push(
+                                      context,
+                                      PageRouteBuilder(
+                                        pageBuilder: (context, animation, secondaryAnimation) {
+                                          return RegistroPage(name: nome, email: mail);
+                                        },
+                                        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                          var begin = Offset(0.0, 1.0);
+                                          var end = Offset.zero;
+                                          var curve = Curves.ease;
+
+                                          var tween = Tween(begin: begin, end: end).chain(
+                                            CurveTween(curve: curve),
+                                          );
+
+                                          return SlideTransition(
+                                            position: animation.drive(tween),
+                                            child: child,
+                                          );
+                                        },
+                                      ),
+                                    );
+                                   */
+                                }
+                              } else {
+                                String nome = '';
+                                String mail = '';
+                                /*  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, animation, secondaryAnimation) {
+                                        return RegistroPage(name: nome, email: mail);
+                                      },
+                                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                        var begin = Offset(0.0, 1.0);
+                                        var end = Offset.zero;
+                                        var curve = Curves.ease;
+
+                                        var tween = Tween(begin: begin, end: end).chain(
+                                          CurveTween(curve: curve),
+                                        );
+
+                                        return SlideTransition(
+                                          position: animation.drive(tween),
+                                          child: child,
+                                        );
+                                      },
+                                    ),
+                                  );
+                                 */
+                              }
+                            },
+                            child: CircleAvatar(
+                              radius: ScreenUtil().setSp(20),
+                              backgroundColor: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: Image.asset('assets/img/google.png'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          InkWell(
+                            onTap: () async {
+                              final user = await Auth.instance.signInWithGoogle(context);
+  final _login = LoginApi();
+                              final ApiModel code = await _login.loginEmail('${user.emailAddress}');
+                              /*   final bottomBloc = ProviderBloc.bottom(context);
+                                bottomBloc.changePage(0); */
+                              if (code.code == '1') {
+                                /*  String token;
+                                    final tokenApi = TokenApi();
+                                    final preferences = Preferences();
+                                    token = await FirebaseMessaging.instance.getToken();
+                                    tokenApi.enviarToken(token);
+                                    preferences.tokenFirebase = token; */
+                                Navigator.of(context).pushNamedAndRemoveUntil('home', (Route<dynamic> route) => false);
+                                //Navigator.pushReplacementNamed(context, 'home');
+
+                              } else {
+                                String nome = '';
+                                String mail = '';
+                                if (user.name != null || user.name!.isNotEmpty) {
+                                  nome = user.name!;
+                                }
+                                if (user.emailAddress != null || user.emailAddress!.isNotEmpty) {
+                                  mail = user.emailAddress!;
+                                }
+                                /* Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, animation, secondaryAnimation) {
+                                        return RegistroPage(name: nome, email: mail);
+                                      },
+                                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                        var begin = Offset(0.0, 1.0);
+                                        var end = Offset.zero;
+                                        var curve = Curves.ease;
+
+                                        var tween = Tween(begin: begin, end: end).chain(
+                                          CurveTween(curve: curve),
+                                        );
+
+                                        return SlideTransition(
+                                          position: animation.drive(tween),
+                                          child: child,
+                                        );
+                                      },
+                                    ),
+                                  ); */
+                              }
+                            },
+                            child: CircleAvatar(
+                              radius: ScreenUtil().setSp(22),
+                              backgroundColor: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: Image.asset('assets/img/google.png'),
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () async {
+                              final user = await Auth.instance.signInWithApple(context);
+
+                              if (user.emailAddress!.isNotEmpty) {
+                                  final _login = LoginApi();
+                                final ApiModel code = await _login.loginEmail('${user.emailAddress}');
+                                /*  final bottomBloc = ProviderBloc.bottom(context);
+                                  bottomBloc.changePage(0); */
+                                if (code.code == '1') {
+                                  /* 
+                                      String token;
+                                      final tokenApi = TokenApi();
+                                      final preferences = Preferences();
+                                      token = await FirebaseMessaging.instance.getToken();
+                                      tokenApi.enviarToken(token);
+                                      preferences.tokenFirebase = token; */
+                                  Navigator.of(context).pushNamedAndRemoveUntil('home', (Route<dynamic> route) => false);
+                                  //Navigator.pushReplacementNamed(context, 'home');
+
+                                } else {
+                                  String nome = '';
+                                  String mail = '';
+                                  if (user.name != null && user.name!.isNotEmpty) {
+                                    nome = user.name!;
+                                  }
+                                  if (user.emailAddress != null && user.emailAddress!.isNotEmpty) {
+                                    mail = user.emailAddress!;
+                                  }
+                                  /* Navigator.push(
+                                      context,
+                                      PageRouteBuilder(
+                                        pageBuilder: (context, animation, secondaryAnimation) {
+                                          return RegistroPage(name: nome, email: mail);
+                                        },
+                                        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                          var begin = Offset(0.0, 1.0);
+                                          var end = Offset.zero;
+                                          var curve = Curves.ease;
+
+                                          var tween = Tween(begin: begin, end: end).chain(
+                                            CurveTween(curve: curve),
+                                          );
+
+                                          return SlideTransition(
+                                            position: animation.drive(tween),
+                                            child: child,
+                                          );
+                                        },
+                                      ),
+                                    ); */
+                                }
+                              } else {
+                                String nome = '';
+                                String mail = '';
+                                /* Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, animation, secondaryAnimation) {
+                                        return RegistroPage(name: nome, email: mail);
+                                      },
+                                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                        var begin = Offset(0.0, 1.0);
+                                        var end = Offset.zero;
+                                        var curve = Curves.ease;
+
+                                        var tween = Tween(begin: begin, end: end).chain(
+                                          CurveTween(curve: curve),
+                                        );
+
+                                        return SlideTransition(
+                                          position: animation.drive(tween),
+                                          child: child,
+                                        );
+                                      },
+                                    ),
+                                  ); */
+                              }
+                            },
+                            child: CircleAvatar(
+                              radius:ScreenUtil().setSp(22),
+                              backgroundColor: Colors.black,
+                              child: SizedBox(
+                                height:ScreenUtil().setSp(25),
+                                child: AspectRatio(
+                                  aspectRatio: 25 / 31,
+                                  child: CustomPaint(
+                                    painter: AppleLogoPainter(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
               ],
             ),
           ),
@@ -258,23 +520,19 @@ class _LoginState extends State<Login> {
               ),
             ),
           ),
-        
-         AnimatedBuilder(
-              animation: _controller,
-              builder: (context, snapshot) {
-                return ShowLoadding(
-                  active: _controller.loadding,
-                );
-              },
-            ),
-            
-            ],
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, snapshot) {
+              return ShowLoadding(
+                active: _controller.loadding,
+              );
+            },
+          ),
+        ],
       ),
     );
   }
 }
-
-
 
 class ControllerLogin extends ChangeNotifier {
   bool loadding = false;
