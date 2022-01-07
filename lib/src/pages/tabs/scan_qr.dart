6a7929019b52f742c6268474aa36e3b60d4404d9
cjunior20 <christianzavaleta20@gmail.com>
2022-01-07 10:52:54 -0500

    .

--- lib/src/pages/tabs/scan_qr.dart
+++ lib/src/pages/tabs/scan_qr.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class ScanQR extends StatelessWidget {
+class ScanQR extends StatefulWidget {
   const ScanQR({Key? key}) : super(key: key);
+
+  @override
+  State<ScanQR> createState() => _ScanQRState();
+}
+
+class _ScanQRState extends State<ScanQR> {
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold();
+  }
+
+  Widget qrView(BuildContext context) {
+    return QRView(key: qrKey, onQRViewCreated: onQRViewCreated);
+  }
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
   }
 }
