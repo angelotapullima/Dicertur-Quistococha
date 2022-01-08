aae208f1c4fa0d586003ea37fc736e8781e28874
cjunior20 <christianzavaleta20@gmail.com>
2022-01-08 11:49:13 -0500

    lenguaje a español

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,23 @@ import 'package:dicertur_quistococha/src/pages/tabs/bloc_contador_qr.dart';
 import 'package:dicertur_quistococha/src/pushProvider/push_notifications.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-
   runZonedGuarded<Future<void>>(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
 
       //
 
-    /*   await loadImage('assets/splash/1K.webp');
+      /*   await loadImage('assets/splash/1K.webp');
       await loadImage('assets/splash/2Y.webp');
       await loadImage('assets/splash/3Y.webp');
       await loadImage('assets/img/pasto2.webp'); */
 
-    /*   final prefs = new Preferences();
+      /*   final prefs = new Preferences();
       final prefsBufiPaymets = new PreferencesBufiPayments(); 
 
       await prefs.initPrefs();
@@ -47,11 +47,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
+    return MultiProvider(
+      providers: [
         ChangeNotifierProvider<ContadorQrBloc>(
           create: (_) => ContadorQrBloc(),
         ),
-        
       ],
       child: ProviderBloc(
         child: ScreenUtilInit(
@@ -60,8 +60,20 @@ class MyApp extends StatelessWidget {
             title: 'Dicertur',
             debugShowCheckedModeBanner: false,
             theme: ThemeData(
-              primarySwatch:Colors.yellow,
+              primarySwatch: Colors.yellow,
             ),
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('es'),
+              Locale('es', 'ES'),
+            ],
+            localeResolutionCallback: (Locale? locale, Iterable<Locale> supportedLocales) {
+              return locale;
+            },
             initialRoute: 'splash',
             routes: {
               'splash': (BuildContext context) => Splash(),
@@ -70,7 +82,7 @@ class MyApp extends StatelessWidget {
               'home': (BuildContext context) => HomePage(),
               //'MetodoPago': (BuildContext context) => MetodoPago(),
             },
-    
+
             //home: MyHomePage(title: 'Flutter Demo Home Page'),
           ),
         ),
